dac1c2d7dcac95b97e1960c45abf9b3144bbd82d
Yurii_Moroziuk <yurii_moroziuk@epam.com>
2023-03-07 12:47:59 +0200

    feature(D-M): create two new types: Ingredient and Recipe. Create many-to-many relation between them.

--- trainingextension/gensrc/com/training/constants/GeneratedTrainingextensionConstants.java
+++ trainingextension/gensrc/com/training/constants/GeneratedTrainingextensionConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Feb 8, 2023, 4:01:22 PM                     ---
+ * --- Generated at Mar 1, 2023, 4:14:50 PM                     ---
  * ----------------------------------------------------------------
  */
 package com.training.constants;
@@ -14,6 +14,19 @@ package com.training.constants;
 public class GeneratedTrainingextensionConstants
 {
 	public static final String EXTENSIONNAME = "trainingextension";
+	public static class TC
+	{
+		public static final String INGREDIENT = "Ingredient".intern();
+		public static final String RECIPE = "Recipe".intern();
+	}
+	public static class Attributes
+	{
+		// no constants defined.
+	}
+	public static class Relations
+	{
+		public static final String INGREDIENT2RECIPERELATION = "Ingredient2RecipeRelation".intern();
+	}
 	
 	protected GeneratedTrainingextensionConstants()
 	{
--- trainingextension/gensrc/com/training/jalo/TrainingextensionManager.java
+++ trainingextension/gensrc/com/training/jalo/TrainingextensionManager.java
@@ -1,18 +1,26 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Feb 8, 2023, 4:01:22 PM                     ---
+ * --- Generated at Mar 1, 2023, 4:14:50 PM                     ---
  * ----------------------------------------------------------------
  */
 package com.training.jalo;
 
 import com.training.constants.TrainingextensionConstants;
+import com.training.jalo.Ingredient;
+import com.training.jalo.Recipe;
 import de.hybris.platform.directpersistence.annotation.SLDSafe;
 import de.hybris.platform.jalo.Item;
 import de.hybris.platform.jalo.Item.AttributeMode;
+import de.hybris.platform.jalo.JaloBusinessException;
 import de.hybris.platform.jalo.JaloSession;
+import de.hybris.platform.jalo.JaloSystemException;
+import de.hybris.platform.jalo.SessionContext;
 import de.hybris.platform.jalo.extension.Extension;
 import de.hybris.platform.jalo.extension.ExtensionManager;
+import de.hybris.platform.jalo.link.Link;
+import de.hybris.platform.jalo.type.ComposedType;
+import de.hybris.platform.jalo.type.JaloGenericCreationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,6 +47,58 @@ public class TrainingextensionManager extends Extension
 			ret.putAll(attr);
 		}
 		return ret;
+	}
+	
+	public Ingredient createIngredient(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType("Ingredient");
+			return (Ingredient)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating Ingredient : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public Ingredient createIngredient(final Map attributeValues)
+	{
+		return createIngredient( getSession().getSessionContext(), attributeValues );
+	}
+	
+	public Recipe createRecipe(final SessionContext ctx, final Map attributeValues)
+	{
+		try
+		{
+			ComposedType type = getTenant().getJaloConnection().getTypeManager().getComposedType("Recipe");
+			return (Recipe)type.newInstance( ctx, attributeValues );
+		}
+		catch( JaloGenericCreationException e)
+		{
+			final Throwable cause = e.getCause();
+			throw (cause instanceof RuntimeException ?
+			(RuntimeException)cause
+			:
+			new JaloSystemException( cause, cause.getMessage(), e.getErrorCode() ) );
+		}
+		catch( JaloBusinessException e )
+		{
+			throw new JaloSystemException( e ,"error creating Recipe : "+e.getMessage(), 0 );
+		}
+	}
+	
+	public Recipe createRecipe(final Map attributeValues)
+	{
+		return createRecipe( getSession().getSessionContext(), attributeValues );
 	}
 	
 	public static final TrainingextensionManager getInstance()
